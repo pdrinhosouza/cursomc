8a627072aae91c6c68c477937c586dc4110bd1a1
Pedro souza <phps86@gmail.com>
2018-08-31 19:13:02 -0300

    Pedido, Estado Pagamento e pagamento

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -37,6 +37,10 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
+	
 	public Cliente() {
 	}
 
@@ -104,7 +108,16 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
 
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -129,6 +142,7 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
+
 	
 
 }

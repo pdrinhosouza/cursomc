53e8dcfa71f8dc41a75021e179424627f6e3a643
Pedro souza <phps86@gmail.com>
2018-09-13 17:55:09 -0300

    inserindo Pedido

--- src/main/java/com/nelioalves/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComCartao.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 
 import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
 
-@Entity
+@Entity 
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
